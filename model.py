73884829d216e1c2fa6ea93d16ca534390c50ebc
dodonator <dodo@chaosdorf.de>
2024-04-27 12:25:35 +0200

    implements basic operations for Item class

--- model.py
+++ model.py
@@ -21,6 +21,14 @@ class Item:
     description: str
     value: int
 
+    def __init__(self, name: str, description: str, value: int) -> None:
+        self.name = name
+        self.description = description
+        self.value = value
+
+    def __repr__(self) -> str:
+        return f"Item({self.name}, {self.description}, {self.value})"
+
 
 class Inventory:
     """An Inventory contains multiple items."""
