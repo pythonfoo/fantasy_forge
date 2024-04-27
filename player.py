b233eb94b72c41d76e2c5efa434f0b9c80d6b94a
dodonator <dodo@chaosdorf.de>
2024-04-27 12:52:11 +0200

    basic Player class

---
+++ player.py
@@ -0,0 +1,21 @@
+from model import Inventory
+
+BASE_INVENTORY_CAPACITY = 10
+
+
+class Player:
+    name: str
+    inventory: Inventory
+
+    def __init__(self, name):
+        self.name = name
+        self.inventory = Inventory(BASE_INVENTORY_CAPACITY)
+
+    def look_at(self, obj: object):
+        """Prints the description of an object."""
+        print(f"{self.name} looks at {obj.name}")
+        print(f"{obj.description}")
+
+    def pick_up(self, item_name: str):
+        self.inventory.add(item_name)
+        print(f"{self.name} picked up {item_name} and put the item in the inventory.")
