42043b9277a1283605f08d5d064c8ec3397f1b02
dodonator <dodo@chaosdorf.de>
2024-04-27 12:41:08 +0200

    implements basic methods for Inventory class

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+from typing import Iterator
+
+
 class NPC:
     """A non player character is a person in the world.
 
@@ -35,3 +38,35 @@ class Inventory:
 
     capacity: int
     contents: list[Item]
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.contents = []
+
+    def add(self, item: Item) -> None:
+        """Adds Item to inventory with respect to capacity."""
+        if len(self) < self.capacity:
+            self.contents.append(item)
+        else:
+            raise Exception(f"Maximum capacity ({self.capacity}) reached.")
+
+    def get(self, item_name: str) -> Item:
+        """Returns item from inventory based on item name."""
+        for item in self.contents:
+            if item.name == item_name:
+                return item
+        raise Exception(f"Item {item_name} couldn't be found.")
+
+    def __len__(self) -> int:
+        """Returns current capacity."""
+        return len(self.contents)
+
+    def __iter__(self) -> Iterator[Item]:
+        """Iterates over items in inventory."""
+        for item in self.contents:
+            yield item
+
+    def __repr__(self) -> str:
+        output: str = f"Inventory({len(self)}/{self.capacity})\n"
+        output += "[" + ", ".join(map(lambda i: i.name, self.contents)) + "]"
+        return output
