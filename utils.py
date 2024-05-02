d3841506753ce3ffff5a9e6c7d440c611f7b08d0
dodonator <dodo@chaosdorf.de>
2024-05-02 18:43:27 +0200

    new file: utils.py

---
+++ utils.py
@@ -0,0 +1,18 @@
+from model import Area, Item
+
+
+def pickup_menu(area: Area) -> Item | None:
+    # filter items contained in area
+    pickup_items: list[Item] = list(
+        filter(lambda c: isinstance(c, Item), area.contents)
+    )
+
+    for idx, item in enumerate(pickup_items):
+        print(f"[{idx:>2}] {item.name}")
+    print("[ q] Quit")
+    selection: str = input("Please select your option: ")
+    if selection.upper() == "Q":
+        return
+    if selection.isnumeric():
+        selection_index = int(selection)
+        return pickup_items[selection_index]
