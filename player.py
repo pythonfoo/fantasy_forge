aba5ed4d1e3620ef3e39402d7fd6ddfaf05ae1d0
dodonator <dodo@chaosdorf.de>
2024-05-03 20:35:38 +0200

    reworks Player.attack

--- player.py
+++ player.py
@@ -1,19 +1,24 @@
 from typing import Self
 
 from character import Character
+from enemy import BASE_DAMAGE
 from inventory import Inventory
 from item import Item
+from weapon import Weapon
 
 BASE_INVENTORY_CAPACITY = 10
 BASE_PLAYER_HEALTH = 100
 
+BARE_HANDS = Weapon("bare hands", "the harmful hands of the player", BASE_DAMAGE)
+
 
 class Player(Character):
-    main_hand: Item
+    main_hand: Item | None
     inventory: Inventory
 
     def __init__(self: Self, name: str, health: int = BASE_PLAYER_HEALTH):
         super().__init__(name, health)
+        self.main_hand = None
         self.inventory = Inventory(BASE_INVENTORY_CAPACITY)
 
     def __repr__(self: Self) -> str:
@@ -36,12 +41,16 @@ class Player(Character):
         print(f"{self.name} equipped {item.name}")
 
     def attack(self, target: Character):
-        """Player attacks Person using his main hand."""
-        print(f"{self.name} attacks {target.name} with {self.main_hand.name}")
-        damage = self.main_hand.damage
-        target.health = target.health - damage
+        """Player attacks character using his main hand."""
+        weapon: Item
+        if self.main_hand is None or not hasattr(self.main_hand, "damage"):
+            weapon = BARE_HANDS
+        else:
+            weapon = self.main_hand
+        print(f"{self.name} attacks {target.name} with {weapon.name}")
+        target.on_attack(weapon)
 
-        if target.health:
+        if target.alive:
             print(f"{target} remains at {target.health} health points")
         else:
             print(f"{target} vanished.")
