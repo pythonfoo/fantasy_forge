d62ea413ad79711f844c7c480c003326ff3bf26c
dodonator <dodo@chaosdorf.de>
2024-05-03 20:25:13 +0200

    adds alive property

--- character.py
+++ character.py
@@ -1,16 +1,26 @@
 from typing import Self
 
 from entity import Entity
+from weapon import Weapon
 
 
 class Character(Entity):
     """A character in the world."""
 
     health: int
+    _alive: bool
 
     def __init__(self: Self, name: str, description: str, health: int) -> None:
         super().__init__(name, description)
         self.health = health
 
     def __repr__(self: Self) -> str:
-        return f"Character({self.name}, {self.description}, {self.health})"
+        return f"Character({self.name}, {self.description}, {self.health}, alive={self.alive})"
+
+    @property
+    def alive(self: Self) -> bool:
+        self._alive = self.health > 0
+        return self._alive
+
+    def on_attack(self: Self, weapon: Weapon):
+        self.health -= weapon.damage
