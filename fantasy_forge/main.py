381683938eeb05d75019d45167512b744da3630f
dodonator <dodo@chaosdorf.de>
2024-05-31 11:04:11 +0200

    implements logger for logging events

--- fantasy_forge/main.py
+++ fantasy_forge/main.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
 from sys import argv
 
+import logging
+
 from .player import Player
 from .world import World
 
@@ -9,12 +11,26 @@ def parse_args(argv=argv[1:]):
     parser = ArgumentParser(description="Fantasy Forge: A text-based RPG")
     parser.add_argument("--version", action="version", version="%(prog)s 0.1.0")
     parser.add_argument("--world", help="The world to play in", default="chaosdorf")
+    parser.add_argument(
+        "--logfile",
+        help="Enables logging for debug purposes",
+        default="fantasy_forge.log",
+    )
+    parser.add_argument("--loglevel", help="Severity Level for logging", default="INFO")
     return parser.parse_args(argv)
 
 
 def main():
     args = parse_args()
+
+    logger = logging.getLogger(__name__)
+    numeric_level = getattr(logging, args.loglevel.upper(), None)
+    logging.basicConfig(filename=args.logfile, level=numeric_level, filemode="w")
+
+    logger.info("load world %s" % args.world)
     world = World.load(args.world)
     player_name = input(world.l10n.format_value("character-name-prompt") + " ")
     player = Player(world, player_name)
+
+    logger.info("starting mainloop for player %s" % player)
     player.main_loop()
