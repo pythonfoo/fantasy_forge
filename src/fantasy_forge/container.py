ed02f514e981bb0fcc81c38e05f437408efb02ac
dodo <dodo@chaosdorf.de>
2024-12-13 19:30:47 +0100

    fixes container class

--- src/fantasy_forge/container.py
+++ src/fantasy_forge/container.py
@@ -1,12 +1,14 @@
-from typing import Self
+from typing import Self, Any
 
 from fantasy_forge.entity import Entity
 from fantasy_forge.inventory import Inventory
+from fantasy_forge.world import World
 
 
 class Container(Entity, Inventory):
     __important_attributes__ = ("name", "capacity")
 
-    def __init__(self: Self, name: str, description: str, capacity: int):
-        super(Container, self).__init__(name, description)
-        super(Entity, self).__init__(capacity)
+    def __init__(self: Self, world: World, config_dict: dict[str, Any]):
+        capacity: int = config_dict.get("capacity", 10)
+        super(Container, self).__init__(world, capacity)
+        super(Entity, self).__init__(world, config_dict)
