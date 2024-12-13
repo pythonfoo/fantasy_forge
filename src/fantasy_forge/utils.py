cdda2d0c25c6797f69c3ba05aa52375a59d6c8db
dodo <dodo@chaosdorf.de>
2024-12-13 20:11:33 +0100

    adds json methods

--- src/fantasy_forge/utils.py
+++ src/fantasy_forge/utils.py
@@ -1,4 +1,8 @@
+import json
+from pathlib import Path
+from typing import IO
 from fantasy_forge.area import Area
+from fantasy_forge.entity import Entity
 from fantasy_forge.item import Item
 
 
@@ -17,3 +21,18 @@ def pickup_menu(area: Area) -> Item | None:
     if selection.isnumeric():
         selection_index = int(selection)
         return pickup_items[selection_index]
+
+def dump_entity(entity: Entity, path: Path):
+    entity_dict = entity.to_dict()
+    assert path.exists()
+    io: IO
+    with path.open("w", encoding="UTF-8") as io:
+        json.dump(entity_dict, io)
+
+def load_entity(path: Path) -> Entity:
+    assert path.exists()
+    io: IO
+    with path.open("w", encoding="UTF-8") as io:
+        entity_dict = json.load(io)
+        entity = Entity(**entity_dict)
+        return entity
