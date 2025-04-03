ba16241d15ab13f82f881d4667df3e527d11fbba
Niklas Sombert <niklas@ytvwld.de>
2025-04-03 21:15:42 +0200

    Load the world and create a Player per connection

--- src/fantasy_forge/multiplayer.py
+++ src/fantasy_forge/multiplayer.py
@@ -1,24 +1,47 @@
 from socket import AF_INET6
-from socketserver import StreamRequestHandler, TCPServer
+from socketserver import StreamRequestHandler, TCPServer, ThreadingMixIn
+from threading import current_thread
+
+from fantasy_forge.player import Player
+from fantasy_forge.world import World
 
 
 class MyTCPHandler(StreamRequestHandler):
     def handle(self):
-        self.data = self.rfile.readline(10000).rstrip()
-        print(f"{self.client_address[0]} wrote:")
-        print(self.data.decode("utf-8"))
-        self.wfile.write(self.data)
+        self.wfile.write(
+            self.server.world.l10n.format_value(
+                "character-name-prompt", {"default": "Player"}
+            ).encode()
+            + b" "
+        )
+        name_input = self.rfile.readline(10000).rstrip().decode()
+        if name_input:
+            player_name = name_input
+        else:
+            player_name = "Player"
+        ip_adress = self.client_address[0]
+        thread = current_thread().getName()
+        print(f"new connection from {player_name} @ {ip_adress} on {thread}")
+        player = Player(self.server.world, player_name)
+        while True:
+            pass
 
 
-class TCPServer6(TCPServer):
+class ThreadedTCPServer6(ThreadingMixIn, TCPServer):
     address_family = AF_INET6
 
 
 def main():
-    HOST, PORT = "::", 9999
+    HOST, PORT = "::", 9998
+    WORLD = "chaosdorf"
+    # TODO: argparse
+
+    # first, create the world
+    world = World.load(WORLD)
 
     # Create the server, binding to localhost on port 9999
-    with TCPServer6((HOST, PORT), MyTCPHandler) as server:
+    with ThreadedTCPServer6((HOST, PORT), MyTCPHandler) as server:
+        server.world = world
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
         server.serve_forever()
